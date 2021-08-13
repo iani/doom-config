882cc12b33d23f741c82400272003417d6298a85
Iannis Zannos <zannos@gmail.com>
2021-08-13 15:24:32 +0900

    initial commit

---
+++ config.el
@@ -0,0 +1,141 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(use-package! org-super-agenda
+  :after org-agenda
+  :init
+
+  (setq
+         org-super-agenda-group-property-name
+         "ProjectId"
+         org-super-agenda-groups '(
+                                   (:auto-group t);
+                                  (:name "Today"
+                                   :time-grid t
+                                   :scheduled today)
+                                  (:name "Due today"
+                                   :deadline today)
+                                  (:name "Important"
+                                   :priority "A")
+                                  (:name "Overdue"
+                                   :deadline past)
+                                  (:name "Due soon"
+                                   :deadline future)
+                                  (:name "Big Outcomes"
+                                   :tag "bo")))
+  :config
+  (org-super-agenda-mode))
+
+(use-package! undo-tree
+  :config
+  (global-undo-tree-mode))
+
+(map! :leader
+      :desc "multi-term"
+      "m t" #'multi-term)
+
+;;; experimental - from :
+;; https://rameezkhan.me/adding-keybindings-to-doom-emacs/
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Note: free <space.-initial keys are:
+;; j, k, l, y, z.
+;; Using:
+;; l -> latex
+;; j -> supercollider 1
+;; ? k -> supercollider 2?
+(map! :leader
+      (:prefix-map ("k" . "scsynth")
+       (:prefix ("j" . "journal")
+        :desc "New journal entry" "j" #'org-journal-new-entry
+        :desc "Search journal entry" "s" #'org-journal-search))
+      (:prefix-map ("j" . "sclang")
+       (:prefix ("l" . "recompile sc-class library")
+        :desc "New journal entry" "j" #'org-journal-new-entry
+        :desc "Search journal entry" "s" #'org-journal-search))
+      (:prefix-map ("l" . "latex")
+       (:prefix ("s" . "subtree")
+        :desc "pdflatex -> pdf" "p" #'org-journal-new-entry
+        :desc "xelatex -> pdf" "x" #'org-journal-new-entry
+        :desc "lualatex -> pdf" "l" #'org-journal-new-entry
+        :desc "edit template" "e" #'org-journal-new-entry
+        :desc "reveal template path" "r" #'org-journal-new-entry
+        :desc "set template path" "t" #'org-journal-search)))
+
+;; more latex commands - find a way to bind them
+;; [s] run script from template dir
+;; [S] edit compile script
+;; beaver / biblatex ...
+;; open last exported pdf
+;; compile index
+;;
+;; supercollider commands - find way to bind them
+;; recompile sc-class library
+;; start supercollider sclang
+;; quit supercollider sclang
+;; command-period
+;; find definition in sc gui.
+;; boot server
+;; quit server
+;; server meter
+;; server audio scope
+;; server control scope
+;; server freq scope
+;; server tree
+;; server kill all servers
+;; trace osc
+;; stop tracing osc
+;; enable osc groups
+;; disable osc groups
+;;
